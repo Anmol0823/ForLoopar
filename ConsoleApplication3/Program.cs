77c6561860839598d6751a2551ce1415ef9587e9
Anmol0823 <anmolratansh@vgy.se>
2016-09-30 14:43:47 +0200

    Alla övn -6

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -10,6 +10,65 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
+            /*
+            // Övning 1
+            for (int tal = 0; tal <= 100; tal++)
+            {
+                Console.WriteLine(tal);
+            }
+            Console.ReadKey();
+
+            //Övning 2
+            for (int tal = 20; tal > 0; tal--)
+            {
+                Console.WriteLine(tal);
+
+            }
+            Console.ReadKey();
+
+            //Övning 3
+            for (int tal = 0; tal <= 50; tal = tal + 2 )
+            {
+                Console.WriteLine(tal);
+            }
+            Console.ReadKey();
+
+            
+
+            //Övning 4
+            Console.WriteLine("Mata in Start");
+           int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Mata in Stopp");
+            int stop = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Mata in steg");
+            int steg = int.Parse(Console.ReadLine());
+
+            var x = start;
+            var y = stop;
+            var z = steg;
+
+            for (start = x; x<=y ; x = x + z)
+            {
+                Console.WriteLine(x + " ");
+            }
+            
+
+            //Övning 5
+            Console.WriteLine("Mata in ett heltal");
+            int heltal = int.Parse(Console.ReadLine());
+            int summa = 0;
+
+            for (int tal = 0; tal <= heltal; tal++)
+            {
+                summa = summa + tal;
+                Console.WriteLine(summa);
+
+            }
+            */
+            //Övning 6
+            Console.WriteLine("Mata in ett Heltal")
         }
     }
 }
